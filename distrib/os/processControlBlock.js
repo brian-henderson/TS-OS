5586bb717416afd0a496a3b9f5191f62ca7533ef
Brian Henderson <brian.henderson1@marist.edu>
2018-10-12 13:20:37 -0400

    Added HTML tables..yes i know still ugly, funcitonality is priority

--- distrib/os/processControlBlock.js
+++ distrib/os/processControlBlock.js
@@ -9,9 +9,35 @@ var TSOS;
         ProcessControlBlock.prototype.getPID = function () {
             return this.pid;
         };
-        ProcessControlBlock.prototype.returnOne = function () {
-            return "hello";
+        ;
+        ProcessControlBlock.prototype.getState = function () {
+            return this.state;
         };
+        ;
+        ProcessControlBlock.prototype.getProgramCounter = function () {
+            return this.programCounter;
+        };
+        ;
+        ProcessControlBlock.prototype.getInstructionReg = function () {
+            return this.instructionReg;
+        };
+        ;
+        ProcessControlBlock.prototype.getAccumulator = function () {
+            return this.accumulator;
+        };
+        ;
+        ProcessControlBlock.prototype.getX = function () {
+            return this.X;
+        };
+        ;
+        ProcessControlBlock.prototype.getY = function () {
+            return this.Y;
+        };
+        ;
+        ProcessControlBlock.prototype.getZ = function () {
+            return this.Z;
+        };
+        ;
         return ProcessControlBlock;
     }());
     TSOS.ProcessControlBlock = ProcessControlBlock;
