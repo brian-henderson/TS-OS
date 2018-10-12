b63a4ba35461b899df908d7c16a17c7c3402e78b
Brian Henderson <brian.henderson1@marist.edu>
2018-10-12 12:50:38 -0400

    Updated PCB

--- source/os/processControlBlock.ts
+++ source/os/processControlBlock.ts
@@ -3,17 +3,50 @@
     module TSOS {
         export class ProcessControlBlock { 
             public pid: number;
+            public state: string;
+            public programCounter : number;
+            public instructionReg: string;
+            public accumulator: number;
+            public X: number;
+            public Y: number;
+            public Z: number;
+            
             constructor(p) {
                 this.pid = p;
             };
 
             public getPID(): number {
                 return this.pid;
-            }
+            };
 
-            public returnOne(): string {
-                return "hello";
-            }
+            public getState(): string {
+                return this.state;
+            };
+
+            public getProgramCounter(): number {
+                return this.programCounter;
+            };
+
+            public getInstructionReg(): string {
+                return this.instructionReg;
+            };
+
+            public getAccumulator(): number {
+                return this.accumulator;
+            };
+
+            public getX(): number {
+                return this.X;
+            };
+
+            public getY(): number {
+                return this.Y;
+            };
+
+            public getZ(): number {
+                return this.Z;
+            };
+
 
         }
 
