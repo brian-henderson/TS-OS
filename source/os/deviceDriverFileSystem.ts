f24fa92144ecfc540c6a4a662b0ee6d9bb3e9600
Brian Henderson <brian.henderson1@marist.edu>
2018-12-08 15:22:47 -0500

    added format code with some comments outlining where to put future code

--- source/os/deviceDriverFileSystem.ts
+++ source/os/deviceDriverFileSystem.ts
@@ -15,12 +15,16 @@
             this.status = "loaded";
          }
 
+         public concatTSB(t, s, b): string {
+            return t.toString() + s.toString() + b.toString();
+         }
+
          public krnFSFormat(): void {
             let inititalTSB: string = "1---MBR";
             let track: number = 0;
             let sector: number = 0;
             let block: number = 0;
-            let tsb = track.toString() + sector.toString() + block.toString();
+            let tsb = this.concatTSB(track, sector, block);
             let emptyTSB: string = '';
 
             for (let i = 0; i < 64; i++) {
@@ -30,6 +34,52 @@
                else {
                   emptyTSB += '0';
                }
+            }
+
+            if (this.formatted) {
+               _HDD.writeToHDD("000", inititalTSB);
+               for(let i = 0; i < _HDD.tsbArray.length; i++) {
+                  _HDD.writeToHDD(i, emptyTSB);
+               }
+               /**
+                * Check processes of ready queue in the resident list
+               */ 
+            }
+            else {
+               for (let i = 0; i <= 999; i++) {
+                  // check to make sure not invalid TSB
+                  if (track == 3 && sector == 7 && block == 8) {
+                     break;
+                  }
+                  // check if tsb is the initial one
+                  if (track == 0 && sector == 0 && block ==0) {
+                     _HDD.writeToHDD(tsb, inititalTSB);
+                     _HDD.tsbArray.push(tsb);
+                     block++;
+                  }
+                  else {
+                     if (block == 8) {
+                        block = 0;
+                        sector++;
+                     }
+                     if (sector == 8) {
+                        block = 0;
+                        sector = 0;
+                        track++;
+                     }
+                     tsb = this.concatTSB(track, sector, block);
+                     _HDD.tsbArray.push(tsb);
+                     _HDD.writeToHDD(tsb, emptyTSB);
+                     block ++;
+                  }
+               /**
+                * Initialize HDD HTML table with data here from TSB
+               */
+               this.formatted = true;
+               }
+               /**
+                * Update the HTML HDD table with all the tsbs that went through loop
+               */
             }
 
             
